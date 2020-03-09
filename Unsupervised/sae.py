7cd3bc9aa9525babb6b532871c73dcf268e2888d
Connor-John <connor.taylor@icloud.com>
2020-03-09 19:54:58 +1300

    Updated SAE
    added test loop

--- Unsupervised/sae.py
+++ Unsupervised/sae.py
@@ -90,12 +90,27 @@ for epoch in range(1, nb_epoch + 1):
             loss = criterion(output, target)
             mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
             loss.backward()
-            train_loss += np.sqrt(loss.data[0] * mean_corrector)
+            train_loss += np.sqrt(loss.data * mean_corrector)
             s += 1.
             optimizer.step()
     print('epoch: ' + str(epoch) + ' | loss: ' + str(train_loss/s))
         
-        
+
+# Testing the SAE
+test_loss = 0
+s = 0.
+for id_user in range(nb_users):
+    input = Variable(training_set[id_user]).unsqueeze(0)
+    target = Variable(test_set[id_user]).unsqueeze(0)
+    if torch.sum(target.data > 0) > 0:
+        output = sae(input)
+        target.require_grad = False
+        output[target == 0] = 0
+        loss = criterion(output, target)
+        mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
+        test_loss += np.sqrt(loss.data * mean_corrector)
+        s += 1.
+print('test loss: '+str(test_loss/s))        
         
         
         
